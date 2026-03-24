68f6121791ab61027bad8ae92b710cc84054f372
Tarikjll <160648106+Tarikjll@users.noreply.github.com>
2026-03-24 23:25:18 +0100

    fix

--- CHEZSWA/CHEZSWA/Comme_Chez_Swa/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Comme_Chez_Swa.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
